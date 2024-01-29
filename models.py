bb75e700647743ffeaf0a588b003e99971f9fe8c
Jukra <lo6ster6@gmail.com>
2024-01-29 23:51:09 +0700

    fix layout

--- models.py
+++ models.py
@@ -8,3 +8,12 @@ class User(UserMixin, db.Model):
     name = db.Column(db.String(100))
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+    
+    # Relationship with cards
+    cards = db.relationship('Card', backref='user', lazy=True)
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
