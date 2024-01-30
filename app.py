0980a93eef7b66878ba6142f2b4102f7bd1b3f0b
Jukra <lo6ster6@gmail.com>
2024-01-30 09:32:50 +0700

    add log out function

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for,flash, get_flashed_messages
+from flask import Flask, render_template, request, redirect, url_for, flash, get_flashed_messages
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from models import db, User
-from flask_login import LoginManager, login_user, login_required, logout_user,current_user
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'your_secret_key_here'
 login_manager = LoginManager(app)
-login_manager.login_view = 'login' 
+login_manager.login_view = 'login'
 login_manager.init_app(app)
 
 db.init_app(app)
@@ -17,11 +17,9 @@ db.init_app(app)
 def engigear():
     return render_template("engigear.html")
 
-
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -33,12 +31,17 @@ def login():
         if user and check_password_hash(user.password, password):
             login_user(user)
             return redirect(url_for('blog'))
-
         else:
             flash('Invalid username or password', 'error')  
             return redirect(url_for('login'))
     flash_messages = list(get_flashed_messages())
     return render_template('Login_card.html', flashed_messages=flash_messages)
+
+@app.route('/logout')  # New route for logout
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('engigear'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -73,7 +76,7 @@ def blog():
 def manage():
     return 'Test route'
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
